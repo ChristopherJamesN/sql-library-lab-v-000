1508bc335dfd1b53c6b5465f78d1ffbf611d2939
Chris Nady <nady.christopher@gmail.com>
2017-09-16 01:59:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -48,5 +48,6 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN books
   ON books.series_id = series.id
   GROUP BY characters.name
+  ORDER BY COUNT(characters.name)
   ;"
 end
